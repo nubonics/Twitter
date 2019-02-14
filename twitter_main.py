581d3b7cafb9c1e674bf73071780331d1f6050d8
nubonics <sparrowsrock@hotmail.com>
2019-02-14 01:54:52 -0800

    Update twitter_main.py

--- twitter_main.py
+++ twitter_main.py
@@ -1,12 +1,19 @@
+import argparse
 from screen_names import sname
 from tweet_dumperv1 import get_all_tweets
 from iter_friends_list import iter_friends_list
 
 
-def main():
-	sname()
+parser = argparse.ArgumentParser()
+parser.add_argument('--screen_name', type=str, help='The screen name of the person you wish to scrape information on.')
+args = parser.parse_args()
+screen_name = args.screen_name
+
+
+def main(screen_name):
+	sname(screen_name)
 	iter_friends_list
 	
 	
 if __name__ == "__main__":
-	main()
+	main(screen_name)
